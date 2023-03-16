11427bd60be203eb373192a6d1e1eb6052a8a7a8
11dpjgailis <janiskrisjanis.g@gmail.com>
2023-03-16 10:42:38 +0200

    characters

---
+++ src/entities/Character.h
@@ -0,0 +1,11 @@
+#ifndef TRIALANDERROR_CHARACTER_H
+#define TRIALANDERROR_CHARACTER_H
+#include <SDL.h>
+class Character{
+public:
+    int x, y;
+    Character(int starting_x, int starting_y);
+};
+
+
+#endif //TRIALANDERROR_CHARACTER_H
