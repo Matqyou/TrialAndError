20604ed3e2620792fe789a18c28c396e1ff562cd
11dpmbozko <matiss2064@gmail.com>
2023-03-16 10:54:28 +0200

    character and resizable window

--- src/entities/Character.cpp
+++ src/entities/Character.cpp
@@ -3,8 +3,19 @@
 //
 
 #include "Character.h"
-Character::Character(int starting_x, int starting_y){
-int x = starting_x;
-int y = starting_y;
-SDL_Rect Draw4 = {x, y, 5, 25};
+Character::Character(SDL_Renderer* Renderer, double starting_x, double starting_y){
+    m_Renderer = Renderer;
+    m_x = starting_x;
+    m_y = starting_y;
+    m_w = 50;
+    m_h = 50;
+}
+
+void Character::Draw() {
+    SDL_SetRenderDrawColor(m_Renderer, rand()%255, rand()%255, rand()%255, 255);
+    SDL_FRect DrawRect = {float(m_x) - float(m_w/2),
+                       float(m_y) - float(m_h/2),
+                       float(m_w),
+                       float(m_h)};
+    SDL_RenderFillRectF(m_Renderer, &DrawRect);
 }
