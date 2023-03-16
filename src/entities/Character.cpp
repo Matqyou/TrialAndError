17d3c865bfac0d45f6a1b9c0eeb421bda5094ce7
11dpmbozko <matiss2064@gmail.com>
2023-03-16 11:23:32 +0200

    unfinished entity & character class

--- src/entities/Character.cpp
+++ src/entities/Character.cpp
@@ -5,5 +5,35 @@
 #include "Character.h"
 Character::Character(SDL_Renderer* Renderer, double start_x, double start_y)
  : Entity(Renderer, start_x, start_y, 50, 50) {
-    // Code for character
+
+    for (bool& State : m_Movement)
+        State = false;
+}
+
+void Character::Tick() {
+    double Speed = 0.05;
+    if (m_Movement[MOVEMENT_UP])
+        m_y -= Speed;
+}
+
+void Character::Event(const SDL_Event& CurrentEvent) {
+    if (CurrentEvent.type == SDL_KEYDOWN ||
+        CurrentEvent.type == SDL_KEYUP) {
+        bool State = CurrentEvent.type == SDL_KEYDOWN;
+        switch (CurrentEvent.key.keysym.scancode) {
+            case SDL_SCANCODE_W: {
+                m_Movement[MOVEMENT_UP] = State;
+            } break;
+            case SDL_SCANCODE_D: {
+                m_Movement[MOVEMENT_RIGHT] = State;
+            } break;
+            case SDL_SCANCODE_S: {
+                m_Movement[MOVEMENT_DOWN] = State;
+            } break;
+            case SDL_SCANCODE_A: {
+                m_Movement[MOVEMENT_LEFT] = State;
+            } break;
+            default: break;
+        }
+    }
 }
--- src/entities/Character.h
+++ src/entities/Character.h
@@ -6,12 +6,21 @@
 
 class Character : public Entity {
 private:
-
+    enum {
+        MOVEMENT_UP,
+        MOVEMENT_RIGHT,
+        MOVEMENT_DOWN,
+        MOVEMENT_LEFT,
+        NUM_MOVEMENTS
+    };
+    bool m_Movement[NUM_MOVEMENTS];
 
 public:
     Character(SDL_Renderer* Renderer, double start_x, double start_y);
 
     // void Draw();
+    void Tick();
+    void Event(const SDL_Event& CurrentEvent);
 };
 
 
