c05ed4a752ce2f82ae6b4e71fa79882c22d83f67
Edijs Aizgirevičs <99726778+ZeUltimatey@users.noreply.github.com>
2023-03-16 18:49:51 +0200

    Controller initialization and events

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ bool Initialize() {
     }
 
     Window = SDL_CreateWindow("TrialAndError", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              600, 600, SDL_WINDOW_RESIZABLE);
+                              900, 700, 0);
     if (!Window) {
         std::printf("Error while creating the window %s", SDL_GetError());
         return false;
@@ -30,11 +30,30 @@ bool Initialize() {
         return false;
     }
 
+    // Initialize up to 3 controllers
+    for (int i = 0; i < 3; i++) {
+        SDL_GameControllerOpen(i);
+    }
+
     Timer = new Clock(60);
     Player = new Character(Renderer, 100, 100);
 
     return true;
 }
+
+
+// Function to handle controller events
+void handleControllerEvent(SDL_Event event)
+{
+    if (event.type == SDL_CONTROLLERBUTTONDOWN) {
+        std::cout << "Button " << static_cast<int>(event.cbutton.button) << " on controller " << static_cast<int>(event.cbutton.which) << " was pressed" << std::endl;
+    } else if (event.type == SDL_CONTROLLERBUTTONUP) {
+        std::cout << "Button " << static_cast<int>(event.cbutton.button) << " on controller " << static_cast<int>(event.cbutton.which) << " was released" << std::endl;
+    } else if (event.type == SDL_CONTROLLERAXISMOTION) {
+        std::cout << "Axis " << static_cast<int>(event.caxis.axis) << " on controller " << static_cast<int>(event.caxis.which) << " moved to " << static_cast<int>(event.caxis.value) << std::endl;
+    }
+}
+
 
 int main() {
     if (!Initialize()) {
@@ -53,7 +72,21 @@ int main() {
                 case SDL_KEYDOWN: {
                     if (CurrentEvent.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
                         Running = false;
+                    else if (CurrentEvent.key.keysym.scancode == SDL_SCANCODE_F11)
+                        SDL_SetWindowFullscreen(Window, !(SDL_GetWindowFlags(Window) & SDL_WINDOW_FULLSCREEN));
                 } break;
+
+                case SDL_CONTROLLERBUTTONDOWN:
+                    std::cout<<"SDL_CONTROLLERBUTTONDOWN ";
+
+                case SDL_CONTROLLERBUTTONUP:
+                    std::cout<<"SDL_CONTROLLERBUTTONUP ";
+
+                case SDL_CONTROLLERAXISMOTION:
+                    std::cout<<"SDL_CONTROLLERAXISMOTION ";
+
+                    handleControllerEvent(CurrentEvent);
+                    break;
             }
         }
 
@@ -70,5 +103,8 @@ int main() {
     delete Timer;
     SDL_DestroyRenderer(Renderer);
     SDL_DestroyWindow(Window);
+    for (int i = 0; i < 3; i++) {
+        SDL_GameControllerClose(SDL_GameControllerFromInstanceID(i));
+    }
     return 0;
 }
