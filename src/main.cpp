6ac43cf1a0cbb488c378b586d676c6f0c3632f44
Edijs Aizgirevičs <99726778+ZeUltimatey@users.noreply.github.com>
2023-03-30 10:38:22 +0300

    cool buttons and files

--- src/main.cpp
+++ src/main.cpp
@@ -18,19 +18,18 @@ bool Initialize() {
     if (!GameWindow->Initialize())
         return false;
 
-    World = new GameWorld(GameWindow, 10000, 10000);
+    World = new GameWorld(GameWindow, 10000, 1000);
     World->SetCameraPos(30, 30);
     GameWindow->RenderClass()->SetWorld(World);
 
     TextManager* TextHandler = GameWindow->TextHandler();
     TTF_Font* Font1 = TextHandler->LoadFont("GROBOLD.ttf", 16);
-    TextTexture = TextHandler->Render(Font1, "get out or -.. .. .", { 255, 255, 255 }, true);
+    TextTexture = TextHandler->Render(Font1, "Jesse -.. .. .", { 255, 255, 255 }, true);
 
     Controllers = new GameControllers();
     new Character(World, 30, 30, 10, 10);
     return true;
 }
-
 
 int main() {
     if (!Initialize()) {
@@ -44,12 +43,18 @@ int main() {
     ImageManager* ImageHandler = GameWindow->ImageHandler();
 
     // Load the PNG images
+    Texture* TextureStart = ImageHandler->LoadTexture("assets/Start.png", true);
+    Texture* TextureSettings = ImageHandler->LoadTexture("assets/Settings.png", true);
     Texture* TextureConnected = ImageHandler->LoadTexture("assets/chain.png", true);
     Texture* TextureDisconnected = ImageHandler->LoadTexture("assets/dis_chain.png", true);
     Texture* TextureIcon = ImageHandler->LoadTexture("assets/PS4_Controller_Icon.png", true);
     Texture* Vignette = ImageHandler->LoadTexture("assets/vignette.png", true);
     Vignette->SetAlpha(200);
 
+    Sound* Background = SoundHandler->LoadSound("assets/background_theme.mp3", true);
+    Sound* Fail_Death = SoundHandler->LoadSound("assets/fail_death.mp3", true);
+    Sound* Basic_Death = SoundHandler->LoadSound("assets/basic_death.wav", true);
+    Sound* Epic_Death = SoundHandler->LoadSound("assets/epic_death.wav", true);
     Sound* LowSound = SoundHandler->LoadSound("assets/Low.wav", true);
     Sound* HighSound = SoundHandler->LoadSound("assets/High.wav", true);
     Sound* QuitSound = SoundHandler->LoadSound("assets/Quit.wav", true);
@@ -58,13 +63,13 @@ int main() {
     SDL_Rect DisconnectedRect = { 200, 375, 80, 44 };
     SDL_Rect IconRect = { 100, 400, 200, 109 };
 
-    // Render the Start button
-    SDL_Rect startButtonRect = { 350, 100, 250, 60 };
-    // Render the Settings button
-    //SDL_Rect settingsButtonRect = { 350, 200, 250, 60 };
-
     bool Running = true;
+    bool m_Config = true;
     while (Running) {
+        // Render the Start button
+        SDL_Rect startButtonRect = { GameWindow->Width()/2-150, GameWindow->Height()/2-200, 300, 100 };
+        // Render the Settings button
+        SDL_Rect settingsButtonRect = { GameWindow->Width()/2-150, GameWindow->Height()/2-50, 300, 100 };
         // Input and events
         SDL_Event CurrentEvent;
         while (SDL_PollEvent(&CurrentEvent)) {
@@ -87,13 +92,13 @@ int main() {
                     GameController* CurrentController = Controllers->OpenController(DeviceID);
                     auto* NewPlayer = new Character(World, 30, 30, 10, 10); // Add new player
                     NewPlayer->SetGameController(CurrentController);
-                    SoundHandler->PlaySound(HighSound);
+                    SoundHandler->PlaySound(Epic_Death);
                 } break;
                 case SDL_CONTROLLERDEVICEREMOVED: {
                     int InstanceID = CurrentEvent.cdevice.which;
                     GameController* DeletedController = Controllers->CloseController(InstanceID);
                     World->DestroyPlayerByController(DeletedController);
-                    SoundHandler->PlaySound(LowSound);
+                    SoundHandler->PlaySound(Fail_Death);
                 } break;
                 case SDL_MOUSEBUTTONDOWN: {
                     if (CurrentEvent.button.button == SDL_BUTTON_LEFT)
@@ -105,11 +110,11 @@ int main() {
                         {
                             World->SetPaused(false);
                         }
-                        //else if (x >= settingsButtonRect.x && x < settingsButtonRect.x + settingsButtonRect.w &&
-                        //    y >= settingsButtonRect.y && y < settingsButtonRect.y + settingsButtonRect.h)
-                        //{
-                        //    std::cout << "Settings" << std::endl;
-                        //}
+                        else if (x >= settingsButtonRect.x && x < settingsButtonRect.x + settingsButtonRect.w &&
+                            y >= settingsButtonRect.y && y < settingsButtonRect.y + settingsButtonRect.h)
+                        {
+                            m_Config = !m_Config;
+                        }
                     }
                 } break;
             }
@@ -140,14 +145,36 @@ int main() {
 
             // start
             Draw->SetColor(90, 20, 20, 255);
-            Draw->FillRect(startButtonRect);
-            // setting
-            //SDL_SetRenderDrawColor(Renderer, 0, 80, 40, 255);
-            //SDL_RenderFillRect(Renderer, &settingsButtonRect);
+            Draw->FillRect({ GameWindow->Width()/2-150, GameWindow->Height()/2-200, 300, 100 });
+            //   setting
+            Draw->SetColor(20, 20, 90, 255);
+            Draw->FillRect({ GameWindow->Width()/2-150, GameWindow->Height()/2-50, 300, 100 });
 
-            Draw->RenderTexture(TextureConnected->SDLTexture(), nullptr, ConnectedRect);
-            Draw->RenderTexture(TextureDisconnected->SDLTexture(), nullptr, DisconnectedRect);
-            Draw->RenderTexture(TextureIcon->SDLTexture(), nullptr, IconRect);
+            Draw->RenderTexture(TextureStart->SDLTexture(), nullptr, startButtonRect);
+            Draw->RenderTexture(TextureSettings->SDLTexture(), nullptr, settingsButtonRect);
+
+            if (m_Config == false) {
+                Draw->SetBlendingMode(SDL_BLENDMODE_BLEND);
+                Draw->SetColor(0, 0, 0, 100);
+                Draw->BlendFullscreen();
+                Draw->SetBlendingMode(SDL_BLENDMODE_NONE);
+
+                //   setting
+                Draw->SetColor(20, 20, 90, 255);
+                Draw->FillRect(settingsButtonRect);
+
+                Draw->RenderTexture(TextureSettings->SDLTexture(), nullptr, settingsButtonRect);
+
+                // for() {
+                //     Draw->RenderTexture(TextureIcon->SDLTexture(), nullptr, IconRect);
+                //     if () {
+                //         Draw->RenderTexture(TextureConnected->SDLTexture(), nullptr, ConnectedRect);
+                //     }
+                //     if () {
+                //         Draw->RenderTexture(TextureDisconnected->SDLTexture(), nullptr, DisconnectedRect);
+                //     }
+                // }
+            }
         }
 
         Draw->UpdateWindow();
