f09791c122b57f8831eca23da8d544d6387b134d
Mim <janiskrisjanis.g@gmail.com>
2025-06-11 19:56:52 +0300

    FIxed issues with wrong state

---
+++ src/game/classes/PlayerClass.cpp
@@ -0,0 +1,27 @@
+#include "PlayerClass.h"
+#include "./ClassTypes/HumanClass.h"
+#include "./ClassTypes/CyborgClass.h"
+#include "./ClassTypes/ZombieClass.h"
+#include "./ClassTypes/VampireClass.h"
+
+#include <algorithm>
+
+PlayerClass *PlayerClass::FromString(const std::string &name)
+{
+    std::string lower = name;
+    std::transform(lower.begin(), lower.end(), lower.begin(), ::tolower);
+
+    if (lower == "human")
+        return new HumanClass();
+    else if (lower == "cyborg")
+        return new CyborgClass();
+    else if (lower == "zombie")
+        return new ZombieClass();
+    else if (lower == "vampire")
+        return new VampireClass();
+    return CreateDefaultClass();
+}
+PlayerClass *PlayerClass::CreateDefaultClass()
+{
+    return new HumanClass();
+}
